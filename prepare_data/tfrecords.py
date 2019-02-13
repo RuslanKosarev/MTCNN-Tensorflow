c541e8bb7797dd4d77b2dcc8b1b74db394dea98a
korus <kosarev@smedx.com>
2019-02-13 11:07:42 +0300

    refactoring

--- prepare_data/tfrecords.py
+++ prepare_data/tfrecords.py
@@ -7,6 +7,8 @@ import numpy as np
 import tensorflow as tf
 from prepare_data.tfrecord_utils import process_image_without_coder, _convert_to_example_simple
 from prepare_data import h5utils
+from train_models.MTCNN_config import config
+
 
 def add_to_tfrecord(writer, filename, data):
     image_data, height, width = process_image_without_coder(filename)
@@ -14,118 +16,60 @@ def add_to_tfrecord(writer, filename, data):
     writer.write(example.SerializeToString())
 
 
-def get_dataset(fname):
+def get_dataset(inpdata):
 
-    positive = h5utils.read(fname, 'positive')
-    negative = h5utils.read(fname, 'negative')
-    part = h5utils.read(fname, 'part')
-    landmark = h5utils.read(fname, 'landmark')
+    outdata = []
 
-    positive = np.random.choice(positive, size=250000)
-    negative = np.random.choice(negative, size=750000)
-    part = np.random.choice(part, size=250000)
-    # landmark = np.random.choice(landmark, size=250000)
+    for sample in inpdata:
+        bbox = dict()
+        bbox['xmin'] = 0
+        bbox['ymin'] = 0
+        bbox['xmax'] = 0
+        bbox['ymax'] = 0
+        bbox['xlefteye'] = 0
+        bbox['ylefteye'] = 0
+        bbox['xrighteye'] = 0
+        bbox['yrighteye'] = 0
+        bbox['xnose'] = 0
+        bbox['ynose'] = 0
+        bbox['xleftmouth'] = 0
+        bbox['yleftmouth'] = 0
+        bbox['xrightmouth'] = 0
+        bbox['yrightmouth'] = 0
 
-    data = []
+        if len(sample) == 6:
+            bbox['xmin'] = float(sample[2])
+            bbox['ymin'] = float(sample[3])
+            bbox['xmax'] = float(sample[4])
+            bbox['ymax'] = float(sample[5])
+        if len(sample) == 12:
+            bbox['xlefteye'] = float(sample[2])
+            bbox['ylefteye'] = float(sample[3])
+            bbox['xrighteye'] = float(sample[4])
+            bbox['yrighteye'] = float(sample[5])
+            bbox['xnose'] = float(sample[6])
+            bbox['ynose'] = float(sample[7])
+            bbox['xleftmouth'] = float(sample[8])
+            bbox['yleftmouth'] = float(sample[9])
+            bbox['xrightmouth'] = float(sample[10])
+            bbox['yrightmouth'] = float(sample[11])
 
-    for dataset in (positive, negative, part, landmark):
-        for sample in dataset:
-            bbox = dict()
-            bbox['xmin'] = 0
-            bbox['ymin'] = 0
-            bbox['xmax'] = 0
-            bbox['ymax'] = 0
-            bbox['xlefteye'] = 0
-            bbox['ylefteye'] = 0
-            bbox['xrighteye'] = 0
-            bbox['yrighteye'] = 0
-            bbox['xnose'] = 0
-            bbox['ynose'] = 0
-            bbox['xleftmouth'] = 0
-            bbox['yleftmouth'] = 0
-            bbox['xrightmouth'] = 0
-            bbox['yrightmouth'] = 0
+        row = dict()
+        row['filename'] = sample[0]
+        row['label'] = int(sample[1])
+        row['bbox'] = bbox
+        outdata.append(row)
 
-            if len(sample) == 6:
-                bbox['xmin'] = float(sample[2])
-                bbox['ymin'] = float(sample[3])
-                bbox['xmax'] = float(sample[4])
-                bbox['ymax'] = float(sample[5])
-            if len(sample) == 12:
-                bbox['xlefteye'] = float(sample[2])
-                bbox['ylefteye'] = float(sample[3])
-                bbox['xrighteye'] = float(sample[4])
-                bbox['yrighteye'] = float(sample[5])
-                bbox['xnose'] = float(sample[6])
-                bbox['ynose'] = float(sample[7])
-                bbox['xleftmouth'] = float(sample[8])
-                bbox['yleftmouth'] = float(sample[9])
-                bbox['xrightmouth'] = float(sample[10])
-                bbox['yrightmouth'] = float(sample[11])
-
-            row = dict()
-            row['filename'] = sample[0]
-            row['label'] = int(sample[1])
-            row['bbox'] = bbox
-
-            data.append(row)
-
-    # with fname.open() as f:
-    #     data = []
-    #
-    #     for line in f.readlines():
-    #         parts = line.strip().split(' ')
-    #
-    #         bbox = dict()
-    #         bbox['xmin'] = 0
-    #         bbox['ymin'] = 0
-    #         bbox['xmax'] = 0
-    #         bbox['ymax'] = 0
-    #         bbox['xlefteye'] = 0
-    #         bbox['ylefteye'] = 0
-    #         bbox['xrighteye'] = 0
-    #         bbox['yrighteye'] = 0
-    #         bbox['xnose'] = 0
-    #         bbox['ynose'] = 0
-    #         bbox['xleftmouth'] = 0
-    #         bbox['yleftmouth'] = 0
-    #         bbox['xrightmouth'] = 0
-    #         bbox['yrightmouth'] = 0
-    #
-    #         if len(parts) == 6:
-    #             bbox['xmin'] = float(parts[2])
-    #             bbox['ymin'] = float(parts[3])
-    #             bbox['xmax'] = float(parts[4])
-    #             bbox['ymax'] = float(parts[5])
-    #         if len(parts) == 12:
-    #             bbox['xlefteye'] = float(parts[2])
-    #             bbox['ylefteye'] = float(parts[3])
-    #             bbox['xrighteye'] = float(parts[4])
-    #             bbox['yrighteye'] = float(parts[5])
-    #             bbox['xnose'] = float(parts[6])
-    #             bbox['ynose'] = float(parts[7])
-    #             bbox['xleftmouth'] = float(parts[8])
-    #             bbox['yleftmouth'] = float(parts[9])
-    #             bbox['xrightmouth'] = float(parts[10])
-    #             bbox['yrightmouth'] = float(parts[11])
-    #
-    #         row = dict()
-    #         # row['filename'] = parts[0]
-    #         row['filename'] = os.path.join(str(fname.parent), plib.Path(parts[0]).parent.name, plib.Path(parts[0]).name)
-    #         row['label'] = int(parts[1])
-    #         row['bbox'] = bbox
-    #
-    #         data.append(row)
-
-    return data
+    return outdata
 
 
-def pnet_tfrecord(tfrecord, h5file, dataset_dir, shuffling=False, seed=None):
-    """Runs the conversion operation.
+def pnet_tfrecord(tfrecord, h5file, outdir, seed=None):
+    """
 
-    Args:
-      dataset_dir: The dataset directory where the dataset is stored.
-      output_dir: Output directory.
+    :param tfrecord:
+    :param h5file:
+    :param seed:
+    :return:
     """
     np.random.seed(seed=seed)
 
@@ -133,21 +77,32 @@ def pnet_tfrecord(tfrecord, h5file, dataset_dir, shuffling=False, seed=None):
     if tfrecord.exists():
         os.remove(str(tfrecord))
 
-    # get data and shuffling.
-    # fname = dataset_dir.joinpath('train_landmark.txt')
-    dataset = get_dataset(h5file)
+    # get data from the h5 file
+    keys = ('positive', 'negative', 'part', 'landmark')
+    # ratios = np.array([1, 3, 1, 1])
+    sizes = np.array([1, 3, 1, np.NaN])*250000
+    tfdata = []
 
-    # if shuffling:
-    #     np.random.shuffle(dataset)
+    for key, size in zip(keys, sizes):
+        data = h5utils.read(h5file, key)
+        if np.isnan(size):
+            # size = config.batch_size - len(tfdata)
+            tfdata += get_dataset(data)
+        else:
+            # size = int(config.batch_size*ratio/ratios.sum())
+            random_sample = np.random.choice(data, size=int(size))
+            tfdata += get_dataset(random_sample)
+
+    np.random.shuffle(tfdata)
 
     with tf.python_io.TFRecordWriter(str(tfrecord)) as writer:
-        for i, data in enumerate(dataset):
-            filename = dataset_dir.joinpath(data['filename'])
-            add_to_tfrecord(writer, filename, data)
+        for i, sample in enumerate(tfdata):
+            filename = outdir.joinpath(sample['filename'])
+            add_to_tfrecord(writer, filename, sample)
 
             if (i+1) % 100 == 0:
-                print('\r{}/{} images have been converted'.format(i+1, len(dataset)), end='')
+                print('\r{}/{} samples have been written.'.format(i+1, len(tfdata)), end='')
 
-    print('\nFinished converting the MTCNN dataset!')
+    print('\rtfrecord file {} has been written, batch size is {}.'.format(tfrecord, config.batch_size))
 
 
