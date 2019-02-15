10e8b3034074b632ff721946e17877f53f45a1b1
korus <kosarev@smedx.com>
2019-02-15 11:26:59 +0300

    refactoring

--- gen_hard_example.py
+++ gen_hard_example.py
@@ -1,9 +1,10 @@
-#coding:utf-8
-import sys
-#sys.path.append("../")
-from prepare_data.utils import convert_to_square
+# coding:utf-8
+__author__ = 'Ruslan N. Kosarev'
 
+import os
+import pathlib as plib
 import numpy as np
+from prepare_data.utils import convert_to_square
 import argparse
 import pickle as pickle
 from train_models.mtcnn_model import PNet, R_Net, O_Net
@@ -11,15 +12,20 @@ from prepare_data.loader import TestLoader
 from Detection.detector import Detector
 from Detection.fcn_detector import FcnDetector
 from Detection.MtcnnDetector import MtcnnDetector
-from utils import *
 from prepare_data.data_utils import *
-#net : 24(RNet)/48(ONet)
-#data: dict()
-from train_models.MTCNN_config import config
+import mtcnn_config
+from prepare_data import h5utils
+from train_models import MTCNN_config
+from prepare_data import ioutils
 
-def save_hard_example(net, data,save_path):
+
+def save_hard_example(net, data, save_path, image_size):
     # load ground truth from annotation file
     # format of each line: image/path [x1,y1,x2,y2] for each gt_box in this image
+
+    positive = []
+    negative = []
+    part = []
 
     im_idx_list = data['images']
     # print(images[0])
@@ -28,18 +34,18 @@ def save_hard_example(net, data,save_path):
 
     print("processing %d images in total" % num_of_images)
 
-    
     # save files
-    neg_label_file = "../../DATA/no_LM%d/neg_%d.txt" % (net, image_size)
-    neg_file = open(neg_label_file, 'w')
+    # neg_label_file = '../data/{}/neg_{}.txt'.format(net, image_size)
+    # neg_file = open(neg_label_file, 'w')
 
-    pos_label_file = "../../DATA/no_LM%d/pos_%d.txt" % (net, image_size)
-    pos_file = open(pos_label_file, 'w')
+    # pos_label_file = '../data/{}/pos_{}.txt'.format(net, image_size)
+    # pos_file = open(pos_label_file, 'w')
 
-    part_label_file = "../../DATA/no_LM%d/part_%d.txt" % (net, image_size)
-    part_file = open(part_label_file, 'w')
-    #read detect result
-    det_boxes = pickle.load(open(os.path.join(save_path, 'detections.pkl'), 'rb'))
+    # part_label_file = '../data/{}/part_{}.txt'.format(net, image_size)
+    # part_file = open(part_label_file, 'w')
+
+    # read detect result
+    det_boxes = pickle.load(open(str(save_path.joinpath('detections.pkl')), 'rb'))
     # print(len(det_boxes), num_of_images)
     print(len(det_boxes))
     print(num_of_images)
@@ -78,17 +84,18 @@ def save_hard_example(net, data,save_path):
             # compute intersection over union(IoU) between current box and all gt boxes
             Iou = IoU(box, gts)
             cropped_im = img[y_top:y_bottom + 1, x_left:x_right + 1, :]
-            resized_im = cv2.resize(cropped_im, (image_size, image_size),
-                                    interpolation=cv2.INTER_LINEAR)
+            resized_im = cv2.resize(cropped_im, (image_size, image_size), interpolation=cv2.INTER_LINEAR)
 
             # save negative images and write label
             # Iou with all gts must below 0.3            
             if np.max(Iou) < 0.3 and neg_num < 60:
                 #save the examples
-                save_file = get_path(neg_dir, "%s.jpg" % n_idx)
+                filename = neg_dir.joinpath("%s.jpg" % n_idx)
                 # print(save_file)
-                neg_file.write(save_file + ' 0\n')
-                cv2.imwrite(save_file, resized_im)
+                # neg_file.write(save_file + ' 0\n')
+                ioutils.write_image(resized_im, filename)
+                negative.append((os.path.join(filename.parent.name, filename.name), 0, 0, 0, 0, 0))
+
                 n_idx += 1
                 neg_num += 1
             else:
@@ -105,27 +112,35 @@ def save_hard_example(net, data,save_path):
 
                 # save positive and part-face images and write labels
                 if np.max(Iou) >= 0.65:
-                    save_file = get_path(pos_dir, "%s.jpg" % p_idx)
-                    pos_file.write(save_file + ' 1 %.2f %.2f %.2f %.2f\n' % (
-                        offset_x1, offset_y1, offset_x2, offset_y2))
-                    cv2.imwrite(save_file, resized_im)
+                    filename = pos_dir.joinpath("%s.jpg" % p_idx)
+                    ioutils.write_image(resized_im, filename)
+
+                    positive.append((os.path.join(filename.parent.name, filename.name), 1,
+                                     offset_x1, offset_y1, offset_x2, offset_y2))
+                    # pos_file.write(save_file + ' 1 %.2f %.2f %.2f %.2f\n' % (offset_x1, offset_y1, offset_x2, offset_y2))
                     p_idx += 1
 
                 elif np.max(Iou) >= 0.4:
-                    save_file = os.path.join(part_dir, "%s.jpg" % d_idx)
-                    part_file.write(save_file + ' -1 %.2f %.2f %.2f %.2f\n' % (
-                        offset_x1, offset_y1, offset_x2, offset_y2))
-                    cv2.imwrite(save_file, resized_im)
+                    filename = part_dir.joinpath("%s.jpg" % d_idx)
+                    ioutils.write_image(resized_im, filename)
+
+                    part.append((os.path.join(filename.parent.name, filename.name), -1,
+                                 offset_x1, offset_y1, offset_x2, offset_y2))
+                    # part_file.write(save_file + ' -1 %.2f %.2f %.2f %.2f\n' % (offset_x1, offset_y1, offset_x2, offset_y2))
                     d_idx += 1
-    neg_file.close()
-    part_file.close()
-    pos_file.close()
+
+    h5file = '../data/24/dbtrainrnet.h5'
+    h5utils.write(h5file, 'positive', np.array(positive, dtype=MTCNN_config.wider_dtype))
+    h5utils.write(h5file, 'negative', np.array(negative, dtype=MTCNN_config.wider_dtype))
+    h5utils.write(h5file, 'part', np.array(part, dtype=MTCNN_config.wider_dtype))
+
+    # neg_file.close()
+    # part_file.close()
+    # pos_file.close()
 
 
-def t_net(prefix, epoch,
-             batch_size, test_mode="PNet",
-             thresh=[0.6, 0.6, 0.7], min_face_size=25,
-             stride=2, slide_window=False, shuffle=False, vis=False):
+def t_net(config, prefix, epoch, batch_size, test_mode="PNet", thresh=(0.6, 0.6, 0.7), min_face_size=25,
+          stride=2, slide_window=False, shuffle=False, vis=False):
 
     detectors = [None, None, None]
     print("Test model: ", test_mode)
@@ -142,108 +157,101 @@ def t_net(prefix, epoch,
         detectors[0] = FcnDetector(PNet, model_path[0])
 
     # load rnet model
-    if test_mode in ["RNet", "ONet"]:
+    if test_mode == 'RNet':
         detectors[1] = Detector(R_Net, 24, batch_size[1], model_path[1])
 
     # load onet model
-    if test_mode == "ONet":
+    if test_mode == 'ONet':
+        detectors[1] = Detector(R_Net, 24, batch_size[1], model_path[1])
         detectors[2] = Detector(O_Net, 48, batch_size[2], model_path[2])
 
-    basedir = '../../DATA/'
-    #anno_file
-    filename = './wider_face_train_bbx_gt.txt'
-    #read anotation(type:dict), include 'images' and 'bboxes'
+    basedir = '../data/'
+    filename = '../data/WIDER_train/wider_face_train_bbx_gt.txt'
     data = read_annotation(basedir, filename)
+    # data['images'] = data['images'][:300]
+    # data['bboxes'] = data['bboxes'][:300]
+
     mtcnn_detector = MtcnnDetector(detectors=detectors, min_face_size=min_face_size,
                                    stride=stride, threshold=thresh, slide_window=slide_window)
-    print("==================================")
-    # 注意是在“test”模式下
-    # imdb = IMDB("wider", image_set, root_path, dataset_path, 'test')
-    # gt_imdb = imdb.gt_imdb()
+
     print('load test data')
     test_data = TestLoader(data['images'])
-    print ('finish loading')
-    #list
-    print ('start detecting....')
-    detections,_ = mtcnn_detector.detect_face(test_data)
-    print ('finish detecting ')
+    print('start detecting....')
+    detections, landmarks = mtcnn_detector.detect_face(test_data)
+    print('finish detecting ')
+
     save_net = 'RNet'
     if test_mode == "PNet":
         save_net = "RNet"
     elif test_mode == "RNet":
         save_net = "ONet"
-    #save detect result
-    save_path = os.path.join(data_dir, save_net)
-    print ('save_path is :')
-    print(save_path)
-    if not os.path.exists(save_path):
-        os.mkdir(save_path)
 
-    save_file = os.path.join(save_path, "detections.pkl")
-    with open(save_file, 'wb') as f:
-        pickle.dump(detections, f,1)
-    print("%s测试完成开始OHEM" % image_size)
-    save_hard_example(image_size, data, save_path)
+    # save detect result
+    save_path = data_dir.joinpath(save_net)
+    print('save_path is :', save_path)
+    if not save_path.exists():
+        save_path.mkdir()
 
+    save_file = save_path.joinpath(save_path, "detections.pkl")
+    print(save_file)
+    with open(str(save_file), 'wb') as f:
+        pickle.dump(detections, f, 1)
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Test mtcnn',
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--test_mode', dest='test_mode', help='test net type, can be pnet, rnet or onet',
-                        default='RNet', type=str)
-    parser.add_argument('--prefix', dest='prefix', help='prefix of model name', nargs="+",
-                        default=['../data/MTCNN_model/PNet_landmark/PNet', '../data/MTCNN_model/RNet_landmark/RNet', '../data/MTCNN_model/ONet_landmark/ONet'],
-                        type=str)
-    parser.add_argument('--epoch', dest='epoch', help='epoch number of model to load', nargs="+",
-                        default=[18, 14, 16], type=int)
-    parser.add_argument('--batch_size', dest='batch_size', help='list of batch size used in prediction', nargs="+",
-                        default=[2048, 256, 16], type=int)
-    parser.add_argument('--thresh', dest='thresh', help='list of thresh for pnet, rnet, onet', nargs="+",
-                        default=[0.3, 0.1, 0.7], type=float)
-    parser.add_argument('--min_face', dest='min_face', help='minimum face size for detection',
-                        default=20, type=int)
-    parser.add_argument('--stride', dest='stride', help='stride of sliding window',
-                        default=2, type=int)
-    parser.add_argument('--sw', dest='slide_window', help='use sliding window in pnet', action='store_true')
-    #parser.add_argument('--gpu', dest='gpu_id', help='GPU device to train with',default=0, type=int)
-    parser.add_argument('--shuffle', dest='shuffle', help='shuffle data on visualization', action='store_true')
-    parser.add_argument('--vis', dest='vis', help='turn on visualization', action='store_true')
-    args = parser.parse_args()
-    return args
+    save_hard_example(24, data, save_path, 24)
 
 
 if __name__ == '__main__':
 
-    net = 'RNet'
+    test_mode = 'PNet'
 
-    if net == "RNet":
-        image_size = 24
-    if net == "ONet":
-        image_size = 48
+    if test_mode == 'PNet':
+        config = mtcnn_config.PNetConfig()
 
-    base_dir = '../../DATA/WIDER_train'
-    data_dir = '../../DATA/no_LM%s' % str(image_size)
+    if test_mode == 'RNet':
+        config = mtcnn_config.RNetConfig()
+
+    if test_mode == 'ONet':
+        config = mtcnn_config.ONetConfig()
+
+    # if net == "RNet":
+    #     image_size = 24
+    # if net == "ONet":
+    #     image_size = 48
+
+    base_dir = plib.Path('../data/WIDER_train').absolute()
+    data_dir = plib.Path('../data/').absolute().joinpath(str(24))
     
-    neg_dir = get_path(data_dir, 'negative')
-    pos_dir = get_path(data_dir, 'positive')
-    part_dir = get_path(data_dir, 'part')
+    pos_dir = data_dir.joinpath('positive')
+    neg_dir = data_dir.joinpath('negative')
+    part_dir = data_dir.joinpath('part')
 
     # create dictionary shuffle
-    for dir_path in [neg_dir, pos_dir, part_dir]:
-        if not os.path.exists(dir_path):
-            os.makedirs(dir_path)
+    for path in [pos_dir, neg_dir, part_dir]:
+        if not path.exists():
+            path.mkdir(parents=True)
 
-    args = parse_args()
+    prefix = (plib.Path(os.pardir).joinpath('mtcnn/PNet/PNet').absolute(),
+              plib.Path(os.pardir).joinpath('mtcnn/RNet/RNet').absolute(),
+              plib.Path(os.pardir).joinpath('mtcnn/ONet/ONet').absolute())
+
+    epochs = (10, 14, 16)
+    batch_size = (2048, 256, 16)
+    thresh = (0.3, 0.1, 0.7)
+    min_face = 20
+    stride = 2
+    slide_window = False
+    shuffle = False
 
     print('Called with argument:')
-    print(args)
-    t_net(args.prefix,#model param's file
-          args.epoch, #final epoches
-          args.batch_size, #test batch_size 
-          args.test_mode,#test which model
-          args.thresh, #cls threshold
-          args.min_face, #min_face
-          args.stride,#stride
-          args.slide_window, 
-          args.shuffle, 
+    #print(args)
+    t_net(config,
+          prefix,#model param's file
+          epochs, #final epoches
+          batch_size, #test batch_size
+          test_mode,#test which model
+          thresh, #cls threshold
+          min_face, #min_face
+          stride,#stride
+          slide_window,
+          shuffle,
           vis=False)
