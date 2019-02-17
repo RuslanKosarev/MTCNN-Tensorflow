f2b3b8b10d10cd57c9cf451e1f680ee20627bb53
korus <kosarev@smedx.com>
2019-02-17 12:47:29 +0300

    ioutils (image loader) has been updated

--- prepare_data/ioutils.py
+++ prepare_data/ioutils.py
@@ -3,6 +3,7 @@ __author__ = 'Ruslan N. Kosarev'
 
 import os
 import cv2
+import time
 
 
 def write_image(image, filename, prefix=None):
@@ -30,22 +31,72 @@ class ImageLoader:
 
     def __init__(self, data, prefix=None, display=100):
         self.counter = -1
+        self.start_time = time.time()
         self.data = data
         self.display = display
-        self.limit = len(data)
+        self.size = len(data)
         self.prefix = str(prefix)
 
     def __next__(self):
-        if self.counter < self.limit:
-            if self.prefix is not None:
-                image = read_image(os.path.join(self.prefix, str(self.data[self.counter])))
-            else:
-                image = read_image(str(self.data[self.counter]))
+        self.counter += 1
 
-            self.counter += 1
-            if self.counter > 0 and self.counter % self.display == 0:
-                print('\rnumber of performed iterations {}/{}'.format(self.counter, self.limit), end='')
+        if self.counter < self.size:
+            if self.counter % self.display == 0:
+                elapsed_time = (time.time() - self.start_time) / 100
+                print('\rnumber of processed images {}/{}, {} seconds for each image'.
+                      format(self.counter, self.size, elapsed_time), end='')
+                self.start_time = time.time()
+
+            image = read_image(str(self.data[self.counter]), prefix=self.prefix)
+
             return image
         else:
-            print('\rnumber of iterations {}'.format(self.limit), end='\n')
+            print('\rnumber of processed images {}'.format(self.size))
             raise StopIteration
+
+    def reset(self):
+        self.counter = -1
+        return self
+
+
+def read_annotation(filename):
+
+    if not filename.exists():
+        raise IOError('file {} does not exist'.format(filename))
+    data = dict()
+    images = []
+    bboxes = []
+    labelfile = open(str(filename), 'r')
+
+    while True:
+        # image path
+        imagepath = labelfile.readline().strip('\n')
+        if not imagepath:
+            break
+        # imagepath = base_dir + '/WIDER_train/images/' + imagepath
+        images.append(imagepath)
+        # face numbers
+        nums = labelfile.readline().strip('\n')
+        # im = cv2.imread(imagepath)
+        # h, w, c = im.shape
+        one_image_bboxes = []
+        for i in range(int(nums)):
+            # text = ''
+            # text = text + imagepath
+            bb_info = labelfile.readline().strip('\n').split(' ')
+            # only need x, y, w, h
+            face_box = [float(bb_info[i]) for i in range(4)]
+            # text = text + ' ' + str(face_box[0] / w) + ' ' + str(face_box[1] / h)
+            xmin = face_box[0]
+            ymin = face_box[1]
+            xmax = xmin + face_box[2]
+            ymax = ymin + face_box[3]
+            # text = text + ' ' + str(xmax / w) + ' ' + str(ymax / h)
+            one_image_bboxes.append([xmin, ymin, xmax, ymax])
+            # f.write(text + '\n')
+        bboxes.append(one_image_bboxes)
+
+    data['images'] = images
+    data['bboxes'] = bboxes
+
+    return data
